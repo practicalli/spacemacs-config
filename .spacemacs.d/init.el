bd4de98276659fa69c5313bbf4ef47ba6e3fa824
jr0cket <john@jr0cket.co.uk>
2020-01-10 13:10:03 +0000

    Tweak of font configuration from template
    Font size suggested to be a decimal number

--- .spacemacs.d/init.el
+++ .spacemacs.d/init.el
@@ -314,9 +314,9 @@ It should only modify the values of Spacemacs settings."
    ;; (default t)
    dotspacemacs-colorize-cursor-according-to-state t
 
-   ;; Default font, or prioritized list of fonts.
+   ;; Default font or prioritized list of fonts.
    dotspacemacs-default-font '("Ubuntu Mono"
-                               :size 24
+                               :size 24.0
                                :weight normal
                                :width normal)
 
