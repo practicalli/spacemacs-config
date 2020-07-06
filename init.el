be86800fa1d95e3cddab673747e482e546a2c71f
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-07-07 00:33:34 +0100

    Theming: set mode line height to 80% for doom-gruvbox-light theme

--- init.el
+++ init.el
@@ -631,14 +631,17 @@ If you are unsure, try setting them in `dotspacemacs/user-config' first."
 
   ;; custom theme modification
   ;; - overriding default height of modeline
-  ;; (setq-default
-  ;;   theming-modifications
-  ;;     '((spacemacs-light
-  ;;         (mode-line :height 0.92)
-  ;;         (mode-line-inactive :height 0.92))
-  ;;       (doom-solarized-light
-  ;;        (mode-line :height 0.92)
-  ;;        (mode-line-inactive :height 0.92))))
+  (setq-default
+    theming-modifications
+      '((spacemacs-light
+          (mode-line :height 0.92)
+          (mode-line-inactive :height 0.92))
+        (doom-solarized-light
+         (mode-line :height 0.92)
+         (mode-line-inactive :height 0.92))
+        (doom-gruvbox-light
+         (mode-line :height 0.80)
+         (mode-line-inactive :height 0.92))))
 
   )  ;; End of dotspacemacs/user-int
 
