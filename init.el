26b597aa78e17bbbee46df4c42e4a6263cfeedf2
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-10-07 16:52:40 +0100

    Spacemacs Home Buffer: Practicalli banner logo

--- init.el
+++ init.el
@@ -328,7 +328,7 @@ It should only modify the values of Spacemacs settings."
    ;; directory. A string value must be a path to an image format supported
    ;; by your Emacs build.
    ;; If the value is nil then no banner is displayed. (default 'official)
-   dotspacemacs-startup-banner 'official
+   dotspacemacs-startup-banner "~/.spacemacs.d/banners/practicalli-logo.svg"
 
    ;; List of items to show in startup buffer or an association list of
    ;; the form `(list-type . list-size)`. If nil then it is disabled.
