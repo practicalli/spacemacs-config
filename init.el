e149aff6966d1c176ec6335eef81ea1d6105d9a6
John Practicalli Stevenson <john@jr0cket.co.uk>
2020-10-07 16:51:10 +0100

    font: use fira code font to use ligatures
    Fira code is larger than Ubuntu mono, so a smaller font size is required

--- init.el
+++ init.el
@@ -382,8 +382,8 @@ It should only modify the values of Spacemacs settings."
    dotspacemacs-colorize-cursor-according-to-state t
 
    ;; Default font or prioritized list of fonts.
-   dotspacemacs-default-font '("Ubuntu Mono"
-                               :size 24.0
+   dotspacemacs-default-font '("Fira Code"
+                               :size 16.0
                                :weight normal
                                :width normal)
 
