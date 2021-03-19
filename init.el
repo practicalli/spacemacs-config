116baf98dce0bcc0fc60258146508303d8b55245
John Practicalli Stevenson <john@jr0cket.co.uk>
2021-03-19 23:36:20 +0000

    keycast: additional package to show key bindings in mode line

--- init.el
+++ init.el
@@ -222,6 +222,7 @@ This function should only modify configuration layer settings."
    ;; `dotspacemacs/user-config'. To use a local version of a package, use the
    ;; `:location' property: '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
+   dotspacemacs-additional-packages '(keycast)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
