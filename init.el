5da988db8470dbfe68d935b411c4e8a1f3e2ff73
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-02-07 01:55:31 +0000

    config; manual activation of completion help

--- init.el
+++ init.el
@@ -51,8 +51,8 @@ This function should only modify configuration layer settings."
                       auto-completion-enable-help-tooltip t
                       auto-completion-enable-snippets-in-popup t
                       auto-completion-enable-sort-by-usage t
-                      auto-completion-idle-delay 0.0
-                      auto-completion-complete-with-key-sequence "fd"
+                      ;; auto-completion-idle-delay 0.0
+                      ;; auto-completion-complete-with-key-sequence "fd"
                       )
 
      ;; To have auto-completion on as soon as you start typing
