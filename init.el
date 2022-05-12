4a2689b2247778f2f5bdfff19942ee2cd011b1a3
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2022-05-12 10:35:34 +0100

    layers: simplify LSP variables, comment defaults
    The Emacs LSP UI package has defaults that are far less intrusive so most of the
    variables in the LSP layer are commented, greatly simplifying the configuration
    of the layer

--- init.el
+++ init.el
@@ -112,47 +112,12 @@ This function should only modify configuration layer settings."
      json
 
      ;; Language server protocol with minimal visual impact
-     ;; https://practicalli.github.io/spacemacs/install-spacemacs/clojure-lsp/lsp-variables-reference.html
+     ;; https://practical.li/spacemacs/install-spacemacs/clojure-lsp/
      (lsp :variables
-          ;; Formatting and indentation - use Cider instead
-          lsp-enable-on-type-formatting t
-          ;; Set to nil to use CIDER features instead of LSP UI
-          lsp-enable-indentation t
-          lsp-enable-snippet t  ;; to test again
-
-          ;; symbol highlighting - `lsp-toggle-symbol-highlight` toggles highlighting
-          ;; subtle highlighting for doom-gruvbox-light theme defined in dotspacemacs/user-config
-          lsp-enable-symbol-highlighting t
-
-          ;; Show lint error indicator in the mode line
-          lsp-modeline-diagnostics-enable t
-          ;; lsp-modeline-diagnostics-scope :workspace
-
-          ;; popup documentation boxes
-          ;; lsp-ui-doc-enable nil          ;; disable all doc popups
-          lsp-ui-doc-show-with-cursor nil   ;; doc popup for cursor
-          ;; lsp-ui-doc-show-with-mouse t   ;; doc popup for mouse
-          ;; lsp-ui-doc-delay 2                ;; delay in seconds for popup to display
-          lsp-ui-doc-include-signature t    ;; include function signature
-          ;; lsp-ui-doc-position 'at-point  ;; top bottom at-point
-          lsp-ui-doc-alignment 'window      ;; frame window
-
-          ;; code actions and diagnostics text as right-hand side of buffer
-          lsp-ui-sideline-enable nil
-          lsp-ui-sideline-show-code-actions nil
-          ;; lsp-ui-sideline-delay 500
-
-          ;; lsp-ui-sideline-show-diagnostics nil
-
-          ;; reference count for functions (assume their maybe other lenses in future)
-          lsp-lens-enable t
-
-          ;; Efficient use of space in treemacs-lsp display
-          treemacs-space-between-root-nodes nil
-
-          ;; Optimization for large files
-          lsp-file-watch-threshold 10000
-          lsp-log-io nil)
+          lsp-ui-doc-enable nil       ;; disable all doc popups
+          lsp-ui-sideline-enable nil  ;; disable sideline bar for less distraction
+          treemacs-space-between-root-nodes nil  ;; no spacing in treemacs views
+          )
 
      (markdown :variables
                markdown-live-preview-engine 'vmd)
@@ -773,11 +738,6 @@ configuration.
 Put your configuration code here, except for variables that should be set
 before packages are loaded."
 
-  ;; LSP  hacking
-  (setq lsp-ui-sideline-enable nil)
-  ;; (setq lsp-ui-sideline-show-code-actions nil)
-
-  (setq lsp-modeline-diagnostics-scope :workspace)
 
   ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Keeping Helm history clean
