eb803c9da48b793e74cff6e1a3be7dca8374127d
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-03-07 13:01:55 +0000

    layer: shell popup using vterm and zsh
    `SPC '` opens an operating system shell in an Emacs popup window, supporting zsh and power10k prompt

--- init.el
+++ init.el
@@ -161,7 +161,8 @@ This function should only modify configuration layer settings."
 
      ;; SPC ' runs shell in a popup buffer
      (shell :variables
-            shell-default-shell 'eshell
+            shell-default-shell 'vterm
+            shell-default-term-shell "/usr/bin/zsh"
             shell-default-height 30
             shell-default-position 'bottom)
 
