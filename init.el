5c9f93a02f4e267776a1416237d094f6807172fb
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-03-07 14:47:55 +0000

    config: revert starup-lists to default value

--- init.el
+++ init.el
@@ -363,8 +363,8 @@ It should only modify the values of Spacemacs settings."
    ;; pair of numbers, e.g. `(recents-by-project . (7 .  5))', where the first
    ;; number is the project limit and the second the limit on the recent files
    ;; within a project.
-   dotspacemacs-startup-lists '((projects . 3)
-                                (todos . 5))
+   dotspacemacs-startup-lists '((recents . 5)
+                                (projects . 7))
 
    ;; True if the home buffer should respond to resize events. (default t)
    dotspacemacs-startup-buffer-responsive t
