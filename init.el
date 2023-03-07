85a1cbe24d5ceed9ea1d4337ef5a40cc7350df04
John Practicalli <250870+practicalli-john@users.noreply.github.com>
2023-03-07 14:03:20 +0000

    theme: streamline theme default selection
    Only include actively used themes and the default spacemacs themes
    
    `, T s` will open one of many themes

--- init.el
+++ init.el
@@ -403,15 +403,10 @@ It should only modify the values of Spacemacs settings."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press `SPC T n' to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-
    dotspacemacs-themes '(doom-gruvbox
                          doom-gruvbox-light
-                         doom-solarized-light
-                         doom-sourcerer
-                         kaolin-valley-dark
-                         doom-solarized-dark
-                         spacemacs-light
-                         spacemacs-dark)
+                         spacemacs-dark
+                         spacemacs-light)
 
    ;; Set the theme for the Spaceline. Supported themes are `spacemacs',
    ;; `all-the-icons', `custom', `doom', `vim-powerline' and `vanilla'. The
